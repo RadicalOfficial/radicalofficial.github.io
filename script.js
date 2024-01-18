bea44174dff6b0f59f3eaab3484ed36b3a88c748
Radical <125277461+RadicalOfficial@users.noreply.github.com>
2024-01-18 14:50:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const years = document.getElementById("yearright");
+const d = new Date().getFullYear();
+
+years.innerHTML = `${d} - <a href='https://scratch.mit.edu/users/Knightbot63/' target='_blank' style="text-decoration: none;">RadicalOfficial</a> &#x2022; <a href='https://github.com/RadicalOfficial/' style="text-decoration: none;">GitHub</a>`;
