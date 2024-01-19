7404de64f97609569ef1a6f63f5f5daa90d85002
Radical <125277461+RadicalOfficial@users.noreply.github.com>
2024-01-18 23:14:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const d = new Date().getFullYear();
 
 years.innerHTML = `${d} - <a href='https://scratch.mit.edu/users/Knightbot63/' target='_blank' style="text-decoration: none;">RadicalOfficial</a> &#x2022; <a href='https://github.com/RadicalOfficial/' style="text-decoration: none;">GitHub</a>`;
 
-let adsWidth = document.getElementByClassName('ads').offsetWidth;
+let adsWidth = document.getElementsByClassName('ads').offsetWidth;
 
 if (adsWidth == 0){
 	console.log("Adblock is detected")
