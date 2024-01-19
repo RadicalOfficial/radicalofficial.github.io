2a614a40a2498a533adba7481298c7aa79f40a92
Radical <125277461+RadicalOfficial@users.noreply.github.com>
2024-01-18 23:13:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let adsWidth = document.getElementByClassName('ads').offsetWidth;
 
 if (adsWidth == 0){
 	console.log("Adblock is detected")
+  years.insertAdjacentHTML("beforebegin", "<h4>Seems like you're using an adblocker. We don't use cookies either but it's fine if you have it on. It's just a test</h4>")
 } else {
 	console.log("Adblock isn't detected")
 }
