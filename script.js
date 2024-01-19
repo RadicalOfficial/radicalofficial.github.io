596a5fc811353e2d5180d7ce02a1c9ef99993600
Radical <125277461+RadicalOfficial@users.noreply.github.com>
2024-01-19 11:55:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const d = new Date().getFullYear();
 
 years.innerHTML = `${d} - <a href='https://scratch.mit.edu/users/Knightbot63/' target='_blank' style="text-decoration: none;">RadicalOfficial</a> &#x2022; <a href='https://github.com/RadicalOfficial/' style="text-decoration: none;">GitHub</a>`;
 
-let adsWidth = document.getElementsByClassName('ads').offsetWidth;
-
-if (adsWidth == 0){
-	console.log("Adblock is detected")
-  years.insertAdjacentHTML("beforebegin", "<h4>Seems like you're using an adblocker. We don't use cookies either but it's fine if you have it on. It's just a test</h4>")
-} else {
-	console.log("Adblock isn't detected")
-}
+setTimeout(function() {
+  var a = document.querySelector('.showads'),
+  b = a ? (a.offsetHeight ? false : true) : true;
+  years.insertAdjacentHTML('beforebegin', "<h4>Seems like you're using an adblocker. You don't have to turn it off, this is for testing purposes.</h4>")
+  console.log('ads blocked?', b)
+}, 200)
