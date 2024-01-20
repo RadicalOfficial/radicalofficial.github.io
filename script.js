0a8c00bf7175501bc8d6ba977f79765047f589fc
Radical <125277461+RadicalOfficial@users.noreply.github.com>
2024-01-19 20:32:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,4 @@ setTimeout(function() {
 }, 200)
 console.log("%cStop!", 'color: red')
 console.log("I'd like to tell you that if you import code, you're not going to have fun in this website. Even if it's just a little CSS change. You got that? Or are you just trying to find secrets?")
+console.warn("Website Error: Everyone, evacuate! Some hackerman managed to get into js console!!! RUN RUN!!!!!!!!! ")
