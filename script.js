4f7bfb6bb5aa117a228be1b23eaa23c369d7e0a6
Knightbot63 <125277461+RadicalOfficial@users.noreply.github.com>
2024-01-26 22:35:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ const d = new Date().getFullYear();
 
 years.innerHTML = `${d} - <a href='https://scratch.mit.edu/users/Knightbot63/' target='_blank' style="text-decoration: none;">RadicalOfficial</a> &#x2022; <a href='https://github.com/RadicalOfficial/' style="text-decoration: none;">GitHub</a>`;
 
-setTimeout(function() {
-  var a = document.querySelector('.showads'),
-  b = a ? (a.offsetHeight ? false : true) : true;
-  years.insertAdjacentHTML('beforebegin', "<h4>Seems like you're using an adblocker. You don't have to turn it off, this is for testing purposes.</h4>")
-  console.log('ads blocked?', b)
-}, 200)
 const latestfollower = document.getElementById("latestfo");
 console.log("Fetching Data... Please wait");
 fetch("https://api.allorigins.win/raw?url=https://api.scratch.mit.edu/users/Knightbot63/followers/?limit=1&offset=0", {
@@ -21,4 +15,5 @@ fetch("https://api.allorigins.win/raw?url=https://api.scratch.mit.edu/users/Knig
 .then(r => {
   console.log('Response', r)
   console.log(r[0].username)
+  latestfollower.innerHTML = r[0].username
 }).catch(error => console.error('Error', error))
