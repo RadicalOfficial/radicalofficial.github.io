0aab78ce03b4b094085f29a7ab23cb848e46f27a
RadicalOfficial <125277461+RadicalOfficial@users.noreply.github.com>
2025-01-16 10:48:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,14 @@
 
 
 
-
+*/
 const years = document.getElementById("yearright");
 const d = new Date().getFullYear();
 const datetimer = new Date()
 let holiday_class = document.getElementById("holiday");
 
 years.innerHTML = `${d} - <a href='https://scratch.mit.edu/users/Knightbot63/' target='_blank' style="text-decoration: none;">RadicalOfficial</a> &#x2022; <a href='https://github.com/RadicalOfficial/' style="text-decoration: none;">GitHub</a>`;
-
+/*
 const latestfollower = document.getElementById("latestfo");
 console.log("Fetching Data... Please wait");
 function updateInfo() {
